e35d1e2f9ca4eb27708bcb72d92892363d2140f4
maraeliza <145588620+maraeliza@users.noreply.github.com>
2024-10-28 16:07:29 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,67 @@
+var dados = {
+  "resumo": {
+    "descricao": "Profissional em desenvolvimento na área de TI, atualmente cursando Análise e Desenvolvimento de Sistemas no IFTM. Com habilidades sólidas em JavaScript, manipulação de dados, criação de APIs e desenvolvimento de interfaces dinâmicas e interativas, busco aplicar meus conhecimentos para construir experiências digitais envolventes. Minha formação como Técnica em Manutenção e Suporte em Informática me proporciona uma base sólida em resolução de problemas e suporte técnico, permitindo uma abordagem focada na otimização da experiência do usuário e na eficácia das soluções desenvolvidas."
+  },
+  "educacao": [
+    {
+      "curso": "Análise e Desenvolvimento de Sistemas",
+      "instituicao": "IFTM",
+      "inicio": "2023-08",
+      "fim": "2026-07"
+    },
+    {
+      "curso": "Técnico em Manutenção e Suporte em Informática",
+      "instituicao": "IFTM",
+      "inicio": "2017-01",
+      "fim": "2020-01"
+    }
+  ],
+  "linguagens": ["python", "javascript", "java", "c"],
+  "banco_dados": {
+    "sql": ["sql developer (oracle)", "mysql workbench"],
+    "nosql": ["firebase realtime database"]
+  },
+  "certificacoes": [
+    "sql com mysql",
+    "bi com excel",
+    "excel",
+    "power bi",
+    "vendas de alta performance"
+  ],
+  "experiencia": [
+    {
+      "cargo": "Analista de Suporte",
+      "empresa": "EasyJur",
+      "periodo": "2024-03 a 2024-10",
+      "atividades": [
+        "extração e análise de dados com python e sql para gerar relatórios e importar dados para o crm via api, reduzindo a taxa de dados incompletos de 40% para menos de 2%.",
+        "desenvolvimento de projeto de suporte preventivo com gráficos de erros do sistema e relatórios automáticos em excel.",
+        "resolução de problemas técnicos na importação de dados, contribuindo para aumentar o nps  (satisfação do cliente) de abaixo de 75 para acima de 90.",
+        "colaboração com advogados e controllers para solução de problemas no software jurídico, atendendo de 40 a 50 tickets semanais com nps superior a 90."
+      ]
+    },
+    {
+      "cargo": "Monitora de Algoritmo",
+      "instituicao": "IFTM",
+      "periodo": "2024-05 a 2024-09",
+      "atividades": [
+        "auxílio aos alunos na lógica de programação, promovendo aprendizado colaborativo.",
+        "identificação e correção de erros em códigos, desenvolvendo habilidades de depuração.",
+        "suporte na resolução de exercícios e criação de materiais práticos.",
+        "esclarecimento de dúvidas, aumentando a confiança dos alunos."
+      ]
+    },
+    {
+      "cargo": "Professora de desenvolvimento web e tecnologia",
+      "instituicao": "Byjus Future School",
+      "publico": "crianças e adolescentes",
+      "periodo": "06/2021 - 03/2024",
+      "atividades": [
+        "ensino de desenvolvimento web com html, css e javascript, além de jquery, ajax e node.js, proporcionando aos alunos uma base sólida para construir sites e aplicações interativas.",
+        "introdução a frameworks modernos, como react e bootstrap, para desenvolver interfaces intuitivas e responsivas.",
+        "instrução em integração de apis e bancos de dados como firebase nosql, ensinando práticas de manipulação e armazenamento de dados.",
+        "análise de dados e introdução à ia em python, abordando bibliotecas como pandas, tensorflow e keras para construir soluções práticas."
+      ]
+    }
+  ]
+}
--- script.js
+++ script.js
@@ -1,5 +1,48 @@
-$(document).ready(()=>{
+$(document).ready(function () {
+  // Carregar os dados do JSON
 
-    
+  $.getJSON("dados.json", function (dadosJson) {
+    montarSite(dadosJson);
+  }).fail(function () {
+    console.error("Erro ao carregar dados.");
+    montarSite(dados);
+  });
+});
+function montarSite(data) {
+  // Exibir Resumo Profissional
+  $("#resumo-profissional").text(data.resumo.descricao);
 
-})
+  // Exibir Educação
+  const educacaoContainer = $("#educacao");
+  data.educacao.forEach((curso) => {
+    const cursoElement = `
+         <div class="card mb-3">
+           <div class="card-body">
+             <h5 class="card-title">${curso.curso}</h5>
+             <p class="card-text">${curso.instituicao}</p>
+             <p class="card-text">${curso.inicio} - ${curso.fim}</p>
+           </div>
+         </div>
+       `;
+    educacaoContainer.append(cursoElement);
+  });
+
+  // Exibir Experiência Profissional
+  const experienciaContainer = $("#experiencia-profissional");
+  data.experiencia.forEach((experiencia) => {
+    const expElement = `
+         <div class="card mb-3">
+           <div class="card-body">
+             <h5 class="card-title">${experiencia.cargo} - ${
+      experiencia.empresa || experiencia.instituicao
+    }</h5>
+             <p class="card-text">${experiencia.periodo}</p>
+             <ul>${experiencia.atividades
+               .map((atividade) => `<li>${atividade}</li>`)
+               .join("")}</ul>
+           </div>
+         </div>
+       `;
+    experienciaContainer.append(expElement);
+  });
+}
